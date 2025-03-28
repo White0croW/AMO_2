a8507113bb14f17ad9cf131dbba9c3c552ef0f44
White0croW <113042470+White0croW@users.noreply.github.com>
2025-03-28 23:06:04 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 python3 -m venv venv  # Создаем виртуальное окружение
-.source venv/bin/activate  # Активируем его
-pip install -r requirments
+source venv/bin/activate  # Активируем его
+pip install -r AMO_2/requirments
 
 echo "---- 1. Генерация данных ----"
-python3 data_generation.py || { echo "Ошибка генерации данных" >&2; exit 1; }
+python3 AMO_2/data_generation.py || { echo "Ошибка генерации данных" >&2; exit 1; }
 
 echo "---- 2. Предобработка данных ----"
-python3 model_preprocessing.py || { echo "Ошибка предобработки" >&2; exit 1; }
+python3 AMO_2/model_preprocessing.py || { echo "Ошибка предобработки" >&2; exit 1; }
 
 echo "---- 3. Обучение модели ----"
-python3 model_training.py || { echo "Ошибка обучения" >&2; exit 1; }
+python3 AMO_2/model_training.py || { echo "Ошибка обучения" >&2; exit 1; }
 
 echo "---- 4. Тестирование модели ----"
-python3 model_testing.py || { echo "Ошибка тестирования" >&2; exit 1; }
+python3 AMO_2/model_testing.py || { echo "Ошибка тестирования" >&2; exit 1; }
 
 echo "Конвейер успешно завершен!"
 
